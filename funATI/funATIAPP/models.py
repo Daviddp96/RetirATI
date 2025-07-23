fff133732862d51054dfad9fe0030c2d46ea1a83
David Delgado <david.delgadop88@gmail.com>
2025-07-23 06:57:06 -0400

    fix django login issue

--- funATI/funATIAPP/models.py
+++ funATI/funATIAPP/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -104,6 +105,7 @@ class UserSettings(models.Model):
     color_theme = models.CharField(max_length=20, choices=COLOR_CHOICES, default='rosado')
     theme_mode = models.CharField(max_length=20, choices=THEME_CHOICES, default='claro')
     email_notifications = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
         return f"Configuración de {self.user.username}"
